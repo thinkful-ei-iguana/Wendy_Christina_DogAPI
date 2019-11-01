9e45464352c9e693a6fb3909c74f03f1f932f7d3
Wendy Bartos <wendyebartos@gmail.com>
2019-11-01 11:34:19 -0700

    added form input

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 'use strict';
 
+//
 function getDogImage() {
-  fetch('https://dog.ceo/api/breeds/image/random')
+  fetch('https://dog.ceo/api/breeds/image/random/4')
     .then(response => response.json())
+ 
     .then(responseJson => console.log(responseJson));
-}
+};
+
+//generate html for the input forms
+
+let formMaker = function () {
+  let form = `
+  <form class="js-dog-img">
+    <input class="dog-img" type="text" placeholder="How many dogs can you handle?"/>
+  </form>
+  `;
+  $('form').prepend(form);
+};
+
+formMaker();
+
+
+
 
 function watchForm() {
   $('form').submit(event => {
